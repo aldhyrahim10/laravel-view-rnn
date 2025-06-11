52f2d383e1df2bc68d7623ecd1aa7679c35ee19b
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2025-06-11 20:24:23 +0700

    First commit

---
+++ app/Filament/Resources/RecordResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RecordResource\Pages;
+use App\Filament\Resources\RecordResource\RelationManagers;
+use App\Models\Record;
+use App\Models\RecordData;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RecordResource extends Resource
+{
+    protected static ?string $model = RecordData::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title')
+                    ->maxLength(255)
+                    ->required(),
+
+                FileUpload::make('attachment')
+                    ->label('Attachment')
+                    ->rules('mimes:csv')
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title'),
+                TextColumn::make('attachment')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageRecords::route('/'),
+        ];
+    }
+}
